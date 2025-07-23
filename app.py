45b6aa7dd8e84db208a523dd7950c3ad4ea0071c
Areeba Khan <reebkhan004@gmail.com>
2025-07-24 00:36:38 +0500

    Api tested successfully

--- app.py
+++ app.py
@@ -1,14 +1,55 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from extensions import db
+from models import Task
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-from models import Task
+db.init_app(app)
 
 @app.route('/')
 def index():
     return "Task API is running!"
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    new_task = Task(title=data['title'], description=data.get('description', ''))
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'message': 'Task created'}), 201
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    result = []
+    for task in tasks:
+        result.append({
+            'id': task.id,
+            'title': task.title,
+            'description': task.description,
+            'done': task.done
+        })
+    return jsonify(result)
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = Task.query.get_or_404(id)
+    data = request.get_json()
+    task.title = data.get('title', task.title)
+    task.description = data.get('description', task.description)
+    task.done = data.get('done', task.done)
+    db.session.commit()
+    return jsonify({'message': 'Task updated'})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({'message': 'Task deleted'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
