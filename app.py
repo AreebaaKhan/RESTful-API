07f21fda2644c4a31d07c7c04e06ae78b4c5c16c
Areeba Khan <reebkhan004@gmail.com>
2025-07-23 16:25:32 +0500

    Added all required  files

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+from models import Task
+
+@app.route('/')
+def index():
+    return "Task API is running!"
